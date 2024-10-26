78ebb03fe9e8ca6721869d0bf454967cbf59c1ae
MohamedMurad368 <mohamedmurad9322@gmail.com>
2024-10-26 22:16:31 +0300

    new update

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -46,18 +46,24 @@ const TaskList = () => {
           onChange={(e) => setSearchTerm(e.target.value)}
           className="border rounded-md p-2 w-full sm:w-1/3 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400"
         />
-        <select value={filterPriority} onChange={(e) => setFilterPriority(e.target.value)} className="border rounded-md p-2 w-full sm:w-auto shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400">
-          <option value="All">All Priorities</option>
-          <option value="Low">Low</option>
-          <option value="Medium">Medium</option>
-          <option value="High">High</option>
-        </select>
-        <select value={filterState} onChange={(e) => setFilterState(e.target.value)} className="border rounded-md p-2 w-full sm:w-auto shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400">
-          <option value="All">All States</option>
-          <option value="todo">To Do</option>
-          <option value="doing">Doing</option>
-          <option value="done">Done</option>
-        </select>
+        
+        {tasks.length > 0 && (
+          <>
+            <select value={filterPriority} onChange={(e) => setFilterPriority(e.target.value)} className="border rounded-md p-2 w-full sm:w-auto shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+              <option value="All">All Priorities</option>
+              <option value="Low">Low</option>
+              <option value="Medium">Medium</option>
+              <option value="High">High</option>
+            </select>
+            <select value={filterState} onChange={(e) => setFilterState(e.target.value)} className="border rounded-md p-2 w-full sm:w-auto shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+              <option value="All">All States</option>
+              <option value="todo">To Do</option>
+              <option value="doing">Doing</option>
+              <option value="done">Done</option>
+            </select>
+          </>
+        )}
+        
         <button onClick={openForm} className="bg-green-500 text-white p-2 rounded-md mt-2 sm:mt-0">
           Add New 
         </button>
@@ -80,30 +86,33 @@ const TaskList = () => {
             </tr>
           </thead>
           <tbody>
-            {filteredTasks.map(task => (
-              <tr key={task.id} className="hover:bg-gray-100 transition duration-150 text-center text-xs sm:text-sm">
-                <td className="py-2 px-2 border-b flex flex-col items-center">
-                  {task.image && (
-                    <>
+            {filteredTasks.length > 0 ? (
+              filteredTasks.map(task => (
+                <tr key={task.id} className="hover:bg-gray-100 transition duration-150 text-center text-xs sm:text-sm">
+                  <td className="py-2 px-2 border-b flex flex-col items-center">
+                    {task.image && (
                       <img src={task.image} alt={task.title} className="w-12 h-12 object-cover rounded-full border border-gray-300 mb-1 sm:w-15 sm:h-15" />
-                    </>
-                  )}
-                </td>
-                <td className="py-2 px-2 border-b">{task.title}</td>
-                
-                <td className="py-2 px-2 border-b">{task.description}</td>
-                <td className="py-2 px-2 border-b">{task.priority}</td>
-                <td className="py-2 px-2 border-b">{task.state}</td>
-                <td className="py-2 px-2 border-b flex justify-center items-center space-x-2">
-                  <button onClick={() => dispatch(deleteTask(task.id))} className="text-red-500 hover:text-red-600 transition duration-150 border-none">
-                    <FaTrash className="w-4 h-4 mb-3" />
-                  </button>
-                  <button onClick={() => startEditing(task)} className="text-yellow-500 hover:text-yellow-600 transition duration-150 border-none">
-                    <FaEdit className="w-4 h-4 mb-3" />
-                  </button>
-                </td>
+                    )}
+                  </td>
+                  <td className="py-2 px-2 border-b">{task.title}</td>
+                  <td className="py-2 px-2 border-b">{task.description}</td>
+                  <td className="py-2 px-2 border-b">{task.priority}</td>
+                  <td className="py-2 px-2 border-b">{task.state}</td>
+                  <td className="py-2 px-2 border-b flex justify-center items-center space-x-2">
+                    <button onClick={() => dispatch(deleteTask(task.id))} className="text-red-500 hover:text-red-600 transition duration-150 border-none">
+                      <FaTrash className="w-4 h-4 mb-3" />
+                    </button>
+                    <button onClick={() => startEditing(task)} className="text-yellow-500 hover:text-yellow-600 transition duration-150 border-none">
+                      <FaEdit className="w-4 h-4 mb-3" />
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="6" className="py-4 text-center">No tasks available</td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
       </div>
